163c64b22fdcf14c7348dad7b5eb3efc4a6144a9
GatoImorrivel <guilhermevianagui23@gmail.com>
2021-11-07 03:28:49 -0300

    Fist Commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Gato's Auto Ricer
+# Yet Another Rice
 This is my script for installing a GUI on Arch. This was made for my own use but feel free to fork it.  
 All of the settings are made with **1920x1080** in mind, functionality in other resolutions is not guaranteed.
 
@@ -7,25 +7,10 @@ All of the settings are made with **1920x1080** in mind, functionality in other 
 - TWM - BSPWM
 - Compositor - Picom
 - App launcher - rofi
-- Widgets & bar - eww
-
-# Screenshots
-<img src='/screenshots/desktop.png'>
-<img src='/screenshots/unixporntypestuff.png'>
-<img src='/screenshots/rofi.png'>
-<img src='/screenshots/powermenu.png'>
+- Bar - PolyBar
 
 # Installing
 
-Just clone it with git and chmod `install-on-arch.sh` file.  
-After that just run the file and wait a little bit.  
-On the rust installation i use the package from the community repo
-
-## Nvim Installation
-All of the **NeoVim** configurations come from my uncle's repo  
-https://github.com/adrianoviana87/vim-pure
-
-### Coc Installation
-    :CocInstall coc-lists  
-    :CocInstall coc-marketplace  
-    :CocList marketplace 
+    chmod +x ./install-on-arch.sh
+    sh install-on-arch.sh
+    await until the tomfoolery ends 
--- install-on-arch.sh
+++ install-on-arch.sh
@@ -1,9 +1,13 @@
+GARKPATH="$(pwd)"
+
 # Update pacman
 echo "Updating pacman, just wait a little"
 sudo pacman --noconfirm -Syu
 
 # Install dependecies
+echo "##############################"
 echo "Installing a bunch of stuff"
+echo "##############################"
 sudo pacman -S --noconfirm --needed xorg xorg-xinit\
     bspwm sxhkd                     \
     lightdm lightdm-gtk-greeter     \
@@ -11,21 +15,16 @@ sudo pacman -S --noconfirm --needed xorg xorg-xinit\
     xwallpaper                      \
     ttf-ubuntu-font-family          \
     rofi                            \
-    papirus-icon-theme              \
-    yarn                            \
-    nodejs                          \
     npm                             \
     xclip                           \
     cmake                           \
-    ttf-nerd-fonts-symbols
 
-# Setup lightdm and nitrogen
+# Setup lightdm
 sudo systemctl enable lightdm &
 
 # Installing NVIM
 sudo pacman -S --noconfirm --needed neovim
 
-# Install rust for alacritty
 echo "##############################"
 echo "installing Rust and alacritty"
 echo "##############################"
@@ -33,81 +32,22 @@ sudo pacman -S --needed rustup cargo
 
 rustup default stable
 
-# Installing paru
-git clone https://aur.archlinux.org/paru.git ./.srcs/paru
-cd ./.srcs/paru
-makepkg -si
+echo "##############################"
+echo "Installing paru"
+echo "##############################"
+git clone https://aur.archlinux.org/paru.git
+cd paru 
+makepkg -si && cd ..
 paru -Syu
 
-# Install picom from jonaburg's repo
+echo "##############################"
+echo "Installing Picom"
+echo "##############################"
 paru -S picom-jonaburg-git
 
-# Installing alacritty
+echo "##############################"
+echo "Installing alacritty"
+echo "##############################"
 paru -S --noconfirm --needed alacritty
 
-# Installing eww
-sudo cp -r ~/GAR/bin/eww /usr/bin/
 
-# Nvim plugin stuff
-paru -S vim-plug
-
-# Copying BSPWM, SXHKD, NVIM, Alacritty and PICOM configs
-mkdir ~/.config
-
-# BSPWM and SXHKD
-mkdir ~/.config/bspwm/
-mkdir ~/.config/sxhkd/
-
-sudo cp -r ~/GAR/configs/bspwm/bspwmrc ~/.config/bspwm/
-sudo cp -r ~/GAR/configs/sxhkd/sxhkdrc ~/.config/sxhkd/
-
-# Alacritty
-mkdir ~/.config/alacritty/
-
-sudo cp -r ~/GAR/configs/alacritty/alacritty.yml ~/.config/alacritty/
-
-sudo rm ~/.bashrc
-
-sudo cp ~/GAR/configs/bashrc/.bashrc ~/.bashrc
-
-# Nvim. Thanks uncle!
-sudo npm install -g neovim
-
-sudo cp -r ~/GAR/configs/nvim/ ~/.config/nvim/
-
-# Picom
-mkdir ~/.config/picom/
-
-sudo cp -r ~/GAR/configs/picom/picom.conf ~/.config/picom/
-
-# Scripts
-sudo cp -r ~/GAR/configs/scripts/ ~/.config/scripts/
-
-sudo chmod 777 ~/.config/scripts/windowswitch.sh
-
-# Eww
-mkdir ~/.config/eww/
-
-sudo cp -r ~/GAR/configs/eww/eww.yuck ~/.config/eww/
-sudo cp -r ~/GAR/configs/eww/eww.scss ~/.config/eww/
-
-# Rofi
-mkdir ~/.config/rofi/
-
-sudo cp -r ~/GAR/configs/rofi/config.rasi ~/.config/rofi/
-sudo cp -r ~/GAR/configs/rofi/launcher.rasi ~/.config/rofi/
-
-# Neofetch
-mkdir ~/.config/neofetch/
-
-sudo cp -r ~/GAR/configs/neofetch/ascii ~/.config/neofetch/
-sudo cp -r ~/GAR/configs/neofetch/config.conf ~/.config/neofetch/
-
-# Wallpaper
-mkdir ~/.config/wallpapers/
-
-sudo cp -r ~/GAR/wallpapers/*.* ~/.config/wallpapers/
-
-# Copying xinit and xprofile
-sudo cp ~/GAR/xconfigs/.xinit ~/
-sudo cp ~/GAR/xconfigs/.xprofile ~/
